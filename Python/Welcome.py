63252b567749c6fd57c903af7df982f641eb0280
Shivtej Patil <shivtej2008@yahoo.com>
2020-06-18 14:00:57 +0100

    hello

--- Python/Welcome.py
+++ Python/Welcome.py
@@ -1,9 +1,7 @@
 import pandas
 
-coronavirusdata=pandas.read_csv("/Users/shivtejpatil/Downloads/COVID-19 Cases.csv")
-pandas.set_option('display.max_columns',None)
-pandas.set_option('display.max_rows',None)
-print(coronavirusdata.groupby(['Country_Region','Date','Case_Type'])['Cases'].sum())
-fsf
+print("hello")
+print("hello")
+
 
 
